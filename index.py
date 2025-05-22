2df08fff73774a03d9d33f9a6efed4786f1f0ef2
simon <simonscheffer00@gmail.com>
2025-05-21 21:32:09 -0300

    concatenar tuplas

--- index.py
+++ index.py
@@ -1,4 +1,9 @@
-animals = ["dog","cat","fish"];
-tuplaAnimals= tuple(animals);
+#animals = ["dog","cat","fish"];
+#tuplaAnimals= tuple(animals);
+#print(len(tuplaAnimals))
 
-print(len(tuplaAnimals))
+days = ("monday","tuesday");
+muchDays = ("wednesday","Thursday");
+
+week = days + muchDays;
+print(week)
