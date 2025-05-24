592a2df26ee5bf1d192d0e5561c0c956d700d0e8
simon <simonscheffer00@gmail.com>
2025-05-24 00:25:22 -0300

    segundo ejercicio

---
+++ guia-5/ejerc1.py
@@ -0,0 +1,21 @@
+listaDelUsuario= [];
+
+pal1=input(" ingrese una palabra  " )
+listaDelUsuario.append(pal1)
+
+pal2=input("ingrese una palabra  " )
+listaDelUsuario.append(pal2);
+pal3=input("ingrese una palabra  ")
+listaDelUsuario.append(pal3)
+pal4=input("ingrese una palabra  ")
+listaDelUsuario.append(pal4);
+pal5=input("ingrese una palabra  ")
+listaDelUsuario.append(pal5)
+
+print(listaDelUsuario)
+
+copiaDeLista = listaDelUsuario;
+copiaDeLista.reverse()
+
+print(copiaDeLista);
+
