a112cc75fcfb8686781ca09ba9d40ec76b298502
simon <simonscheffer00@gmail.com>
2025-05-26 00:21:00 -0300

    easy

---
+++ guia-5/ejer6.py
@@ -0,0 +1,13 @@
+"""Dadas dos listas a y b con tres elementos cada una, calcular: 
+la suma de los primeros elementos, 
+la diferencia entre los segundos, 
+el producto de los terceros. """
+
+lista_a = [3,4,5];
+lista_b=[2,6,8];
+
+suma = lista_a[0] + lista_b[0]
+diferencia = lista_a[1] / lista_b[1]
+producto= lista_a[2] * lista_b[2]
+
+print(f"la suma de los primeros elementos es de : {suma}, la diferencia de los segundos elementos es de : {diferencia} y el producto de lso ultimos elementos es de : {producto}")
