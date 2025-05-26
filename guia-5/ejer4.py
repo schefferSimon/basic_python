f44c02c14ff13db3ed625471cbd375bf1ddeb820
simon <simonscheffer00@gmail.com>
2025-05-26 00:02:21 -0300

    numero de veces de vocales en una palabra

---
+++ guia-5/ejer4.py
@@ -0,0 +1,6 @@
+""" Desarrollar un programa en Python que pida al usuario una palabra y muestre 
+por pantalla el número de veces que contiene cada vocal. """
+
+string= input("ingrese una palabra "  ).lower();
+vocal = string.count("a")
+print(vocal)
