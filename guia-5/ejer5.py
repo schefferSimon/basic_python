cbe1e436d2a1b2d9d2c78b6682dfacbe7d4836c6
simon <simonscheffer00@gmail.com>
2025-05-26 00:08:59 -0300

    longitud de palabras en una lista

---
+++ guia-5/ejer5.py
@@ -0,0 +1,11 @@
+""" Desarrollar un programa en Python que pida al usuario una palabra y muestre 
+por pantalla el número de veces que contiene cada vocal. """
+
+string1= input("ingrese una palabra  ")
+string2= input("ingrese una palabra  ")
+string3= input("ingrese una palabra  ")
+
+longitud =[len(string1), len(string2), len(string3)];
+
+print(f"primera longitud : {longitud[0]} , segunda longitud: {longitud[1]}, tercera longitud: {longitud[2]}")
+
