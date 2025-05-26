4503c09b5b8b5eda4722a74408e0fee9788999d1
simon <simonscheffer00@gmail.com>
2025-05-26 00:47:38 -0300

    remplazar valoresd e una lista

---
+++ guia-5/ejer7.py
@@ -0,0 +1,9 @@
+"""Crear una lista datos = [10, 20, 30, 40, 50]. Reemplazar el primer valor por 100, 
+el tercero por 300 y el último por 500. Mostrar la lista resultante."""
+datos = [10, 20, 30, 40, 50];
+
+datos[0] = 100;
+datos[2]=300;
+datos[-1] = 500;
+
+print(datos)
