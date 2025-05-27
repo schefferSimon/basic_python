d35b9c85dbf8cc200c84927f3fc871c311385a16
simon <simonscheffer00@gmail.com>
2025-05-27 02:02:16 -0300

    teriminar

--- guia-v1/ejer3.py
+++ guia-v1/ejer3.py
@@ -9,5 +9,3 @@ chef3 =set(chef3)
 ingredientes_comunes = chef1 & chef2 & chef3;
 print("Los ingredientes que usan en comun los chef son : ", ingredientes_comunes)
 
-diferencia_uno = [chef1 - chef2- chef3];
-print(diferencia_uno)
