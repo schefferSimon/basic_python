b4c08d16c11be9cc34aa901c9f413b5a1b5a9baa
simon <simonscheffer00@gmail.com>
2025-05-30 01:35:13 -0300

    terminar

---
+++ ejer5.py
@@ -0,0 +1,8 @@
+"""- Crear un diccionario con tuplas como claves: usar coordenadas (latitud,
+longitud) como claves y nombres de ciudades como valores. Mostrar la
+ciudad asociada a una coordenada dada.
+"""
+
+tupla1=(12,32)
+tupla2=(34,54)
+
